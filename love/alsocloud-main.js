db107c8dccbff6065d4e1fdc31b7f8286db5ea16
byyuu <116348696+byyuu@users.noreply.github.com>
2022-10-23 11:46:29 +0800

    Add files via upload

---
+++ love/alsocloud-main.js
@@ -0,0 +1,5 @@
+console.log("\n %c Made with love %c by YiYun | alsocloud.cn \n","color: #444; background:#a7a7f1; padding:5px 0; font-size:12px;","color:#F8F8FF;background:#F4A7B9; padding:5px 0; font-size:12px;");
+
+
+console.log("\n %c AlsoCloud版权所有 %c 如有盗用必承担法律责任，扒站/盗用者请自便 \n","color: #fadfa3; background: #030307; padding:5px 0; font-size:12px;","background: #fadfa3; padding:5px 0; font-size:12px;");
+
